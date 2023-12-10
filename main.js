24ac2017692f4de5ea79520178be333c604502ea
R1verb <128699019+Miqri@users.noreply.github.com>
2023-12-10 23:11:04 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,55 @@ $(document).ready(function() {
         $("body").toggleClass("lock");
     })
 })
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('button');
+
+    // Добавляем обработчики событий на каждую кнопку
+    buttons.forEach(function(button, index) {
+        button.addEventListener('click', function () {
+            activateButton(index);
+        });
+    });
+
+    // Функция для активации кнопки
+    function activateButton(index) {
+        // Деактивируем все кнопки
+        buttons.forEach(function(button) {
+            button.classList.remove('active');
+        });
+
+        // Активируем выбранную кнопку
+        buttons[index].classList.add('active');
+
+        // Ваш дополнительный код при активации кнопки
+        // Например, изменение содержимого какого-то элемента
+        var resultElement = document.getElementById('result');
+        resultElement.textContent = 'Кнопка ' + (index + 1) + ' активирована!';
+    }
+});
+
+
+
+$(document).ready(function() {
+    // Устанавливаем активное состояние по умолчанию для .reproduct__block__info1
+    $(".reproduct__block__info1").addClass("active");
+
+    // ... (ваш существующий код)
+
+    $(".reproduct__france").click(function(event) {
+        $(".reproduct__block__info1").addClass("active");
+        $(".reproduct__block__info2, .reproduct__block__info3").removeClass("active");
+    });
+
+    $(".reproduct__germany").click(function(event) {
+        $(".reproduct__block__info2").addClass("active");
+        $(".reproduct__block__info1, .reproduct__block__info3").removeClass("active");
+    });
+
+    $(".reproduct__england").click(function(event) {
+        $(".reproduct__block__info3").addClass("active");
+        $(".reproduct__block__info1, .reproduct__block__info2").removeClass("active");
+    });
+});
